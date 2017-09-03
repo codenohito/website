59948aa380eb5107c933f4a4e725eae140572fa5
Ivan Dymkov <mstrdymio@gmail.com>
2017-09-03 13:15:28 +0700

    Update team members

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,4 @@ echo "Build production version..."
 npm run build
 echo "Sync the build on a production server..."
 rsync -avzP dist/ dymio@codenohito.server:~/www/codenohito_website
-ssh dymio@codenohito.server 'cd ~/www/codenohito_www/public; git pull'
 echo "--- Deploy complete ---"
